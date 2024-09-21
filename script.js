093f68272412ed6493ce2e9e315096c6e70b2ce1
hmMythreya <hm.mythreya@gmail.com>
2024-09-21 19:10:29 -0400

    added animations

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-document.getElementById('contact-form').addEventListener('submit', function (e) {
-    e.preventDefault();
-    alert('Thank you for reaching out!');
+document.addEventListener("DOMContentLoaded", () => {
+    const bioText = "I’m a cybersecurity student with a passion for protecting and securing digital systems. Here you can find my projects, writeups, and more about my journey.";
+    
+    let typingElement = document.getElementById('typing-text');
+    typingElement.innerText = bioText;
 });
