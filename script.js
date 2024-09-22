4eb95b98ab63d9c7516894280da839e7e0389841
hmMythreya <hm.mythreya@gmail.com>
2024-09-21 21:23:42 -0400

    somewhat working portfolio

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const bioText = "I’m a cybersecurity student with a passion for protecting and securing digital systems. Here you can find my projects, writeups, and more about my journey.";
     
-    let typingElement = document.getElementById('typing-text');
-    typingElement.innerText = bioText;
 });
